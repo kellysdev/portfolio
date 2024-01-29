99d50dae4ec613f5dbd16b7d3978cb3947210b3c
Kelly Scott <116105463+lladysmall@users.noreply.github.com>
2024-01-29 13:43:46 -0800

    Skeleton jsx for header component

---
+++ src/components/header.js
@@ -0,0 +1,22 @@
+const Header = () => {
+  return (
+    <nav className="navbar navbar-expand-lg">
+      <div className="container-fluid">
+        <a className="nav-brand" href="index.html">kelly scott</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggle-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <a className="nav-link" href="portfolio.html">portfolio</a>
+            <a className="nav-link" href="resume.html">resume</a>
+            <a className="nav-link" href="about.html">about</a>
+            <a className="nav-link" href="contact.html">contact</a> 
+          </div>
+        </div>
+      </div>    
+    </nav>
+  );
+};
+
+export default Header;
