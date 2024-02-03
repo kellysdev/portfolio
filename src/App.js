76728eafba0caadeb65139b43d6fb16a0d30b399
Kelly Scott <116105463+lladysmall@users.noreply.github.com>
2024-02-03 11:45:23 -0800

    Implement react-router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/header';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <BrowserRouter>
+      <Routes>
+
+        <Route
+          path="/"
+          element={
+            <div className="App">
+              <Header />
+            </div>
+          }
+        />
+
+        <Route 
+          path="/resume"
+          element={
+            <>
+              <Header />
+              <button>Download Resume</button>
+            </>
+          }
+        />
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,20 @@
+import {Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
 const Header = () => {
   return (
     <nav className="navbar navbar-expand-lg">
       <div className="container-fluid">
-        <a className="nav-brand" href="index.html">kelly scott</a>
+        <a className="nav-brand" href="/">kelly scott</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggle-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div className="navbar-nav">
-            <a className="nav-link" href="#">portfolio</a>
-            <a className="nav-link" href="#">resume</a>
-            <a className="nav-link" href="#">about</a>
-            <a className="nav-link" href="#">contact</a> 
+            <Nav.Link as={Link} to="/" className="nav-link">portfolio</Nav.Link>
+            <Nav.Link as={Link} to="/resume" className="nav-link">resume</Nav.Link>
+            <Nav.Link as={Link} to="/" className="nav-link">about</Nav.Link>
+            <Nav.Link as={Link} to="/" className="nav-link">contact</Nav.Link> 
           </div>
         </div>
       </div>    
