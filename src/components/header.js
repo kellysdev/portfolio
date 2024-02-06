8f8ca30d17b263a8cdb871f195b817b088be106b
Kelly Scott <116105463+lladysmall@users.noreply.github.com>
2024-02-05 17:23:25 -0800

    nav links justify-content-end

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,18 @@
-import {Nav } from "react-bootstrap";
+import Navbar from "react-bootstrap/Navbar";
+import { Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <nav className="navbar navbar-expand-lg">
       <div className="container-fluid">
-        <Nav.Link className="nav-brand" href="/">kelly scott</Nav.Link>
+        <Navbar.Brand>kelly scott</Navbar.Brand>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggle-icon">
             <i className="bi bi-list"></i>
           </span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
           <div className="navbar-nav">
             <Nav.Link as={Link} to="/" className="nav-link">portfolio</Nav.Link>
             <Nav.Link as={Link} to="/resume" className="nav-link">resume</Nav.Link>
