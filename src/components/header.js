c5ba47492f7b6eb78863da3f1eee62a391f11232
Kelly Scott <116105463+lladysmall@users.noreply.github.com>
2024-02-05 17:46:02 -0800

    nav bar collapses and nav items appear after clicking menu button

--- src/components/header.js
+++ src/components/header.js
@@ -1,26 +1,32 @@
 import Navbar from "react-bootstrap/Navbar";
-import { Nav } from "react-bootstrap";
+import Nav from "react-bootstrap/Nav";
 import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <nav className="navbar navbar-expand-lg">
-      <div className="container-fluid">
+    <Navbar expand="md" className="navbar">
         <Navbar.Brand>kelly scott</Navbar.Brand>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link as={Link} to="/" className="nav-link">portfolio</Nav.Link>
+            <Nav.Link as={Link} to="/resume" className="nav-link">resume</Nav.Link>
+            <Nav.Link as={Link} to="/contact" className="nav-link">contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+
+        {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggle-icon">
             <i className="bi bi-list"></i>
           </span>
         </button>
+
         <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
           <div className="navbar-nav">
-            <Nav.Link as={Link} to="/" className="nav-link">portfolio</Nav.Link>
-            <Nav.Link as={Link} to="/resume" className="nav-link">resume</Nav.Link>
-            <Nav.Link as={Link} to="/contact" className="nav-link">contact</Nav.Link> 
           </div>
-        </div>
-      </div>    
-    </nav>
+        </div> */}   
+    </Navbar>
   );
 };
 
