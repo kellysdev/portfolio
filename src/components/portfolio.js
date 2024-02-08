3781583bd50a960b82c86fc6b50faeaa9e6dddb7
Kelly Scott <116105463+lladysmall@users.noreply.github.com>
2024-02-08 09:08:35 -0800

    Add bottom margin to portfolio row

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -11,7 +11,8 @@ const Portfolio = () => {
           Portfolio
         </h1>
       </Row>
-      <Row xs={1} md={2} lg={3} xl={4} className="g-4">
+
+      <Row xs={1} sm={2} lg={3} xl={4} className="mb-5" >
         <Col>
           <Card>
             <Card.Body>
