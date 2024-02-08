bb62d2f9b1b0b2100a5208eae3fde0a654d735b7
Kelly Scott <116105463+lladysmall@users.noreply.github.com>
2024-02-08 10:37:08 -0800

    center download button

--- src/components/resume-page.js
+++ src/components/resume-page.js
@@ -7,7 +7,7 @@ const ResumePage = () => {
     <Row>
       <Col>
         <Row>
-          <Button className="download-button mt-5 mx-5" type="button" variant="outline-dark"
+          <Button className="download-button mt-5 mx-auto" type="button" variant="outline-dark" style={{width: 200}}
             href={require("../Scott-Kelly-Resume.pdf")} download="Scott-Kelly-Resume.pdf"
           ><i className="bi bi-download"></i> Download Resume</Button>
         </Row>
