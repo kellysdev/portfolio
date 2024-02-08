149d1317b47190128649e58b43c47d0fbb7819d1
Kelly Scott <116105463+lladysmall@users.noreply.github.com>
2024-02-08 09:26:24 -0800

    Hash link to portfolio section of homepage

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,19 @@
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import { Link } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
 
 const Header = () => {
   return (
     <Navbar expand="md" className="navbar">
-        <Navbar.Brand>kelly scott</Navbar.Brand>
+        <Navbar.Brand>
+          <Nav.Link as={Link} to="/" className="nav-link">kelly scott</Nav.Link>
+        </Navbar.Brand>
 
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link as={Link} to="/" className="nav-link">portfolio</Nav.Link>
+            <Nav.Link as={HashLink} to="/#portfolio" className="nav-link">portfolio</Nav.Link>
             <Nav.Link as={Link} to="/resume" className="nav-link">resume</Nav.Link>
             <Nav.Link as={Link} to="/about" className="nav-link">about</Nav.Link>
             <Nav.Link as={Link} to="/contact" className="nav-link">contact</Nav.Link>
