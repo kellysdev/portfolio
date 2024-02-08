ba60fea48738b9581c9d5bef187bb33b1e5514c4
Kelly Scott <116105463+lladysmall@users.noreply.github.com>
2024-02-07 19:13:20 -0800

    Hero image works as well as carousel for wireframes

--- src/components/movie-case-study.js
+++ src/components/movie-case-study.js
@@ -1,31 +1,43 @@
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Carousel from "react-bootstrap/Carousel";
 
 const MovieCaseStudy = () => {
   return (
     <Row>
       <Col>
-      
+                {/* hero */}
         <Row>
-          <img className="mcs-hero" src="https://www.flickr.com/photos/vancouver-archives/32350611738" alt="black and white vintage theater"/>
-          <Col>
-            <h1>Case Study</h1>
-              <p>Career Foundry Achievement Project 3<br/>
-              PopOpolis Movie Application</p>
-          </Col>
+          <header className="hero has-bg-img" style={{ paddingLeft: 0 }}>
+            <div className="p-3 text-center bg-image">
+              {/* <div className="mask" style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)' }}> */}
+              <div className="mask">
+                <div className="d-flex justify-content-center align-items-center h-100">
+                  <div className="text-white">
+                    <h1 className="mt-5 text-warning">Case Study</h1>
+                    <h4 className="mb-5 text-warning">React CRUD Movie Application</h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </header>
         </Row>
 
-        <Row>
+        <Row className="mt-4">
           <Col>
             <h3>Overview</h3>
             <p>
               This project is a single-page, responsive app with several interface views and a polished user experience.
             </p>
+          </Col>
+          <Col>
             <h3>Purpose & Context</h3>
             <p>
               This app was built to add to my portfolio while taking the CareerFoundry Full-Stack Web Development course.  It was instrumental in learning to utilize the MERN tech stack.
             </p>
           </Col>
+        </Row>
+        <Row className="mb-4">
           <Col>
             <h3>Objective</h3>
             <p>
@@ -34,7 +46,9 @@ const MovieCaseStudy = () => {
           </Col>
         </Row>
 
-        <Row>
+        <Row className="bg-warning py-1"></Row>
+
+        <Row className="my-5">
           <Col>
             <h3>Developement Steps</h3>
             <ol>
@@ -44,10 +58,9 @@ const MovieCaseStudy = () => {
               <li>Deploy</li>
             </ol>
           </Col>
-          <Col></Col>
         </Row>
 
-        <Row>
+        <Row className="my-5">
           <h3>1 - Planning</h3>
           <Col>
             <h4>Goal:</h4>
@@ -63,11 +76,42 @@ const MovieCaseStudy = () => {
           </Col>
         </Row>
 
-        <Row>
-          {/* wirefram carousel */}
+        <Row className="my-5">
+          <Carousel>
+            <Carousel.Item>
+              <img className="carousel-img" src="wireframe-home.png" alt="wireframe for main view" />
+              <Carousel.Caption>
+                <h4>Main View</h4>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="carousel-img" src="wireframe-movie.png" alt="wireframe for movie view"/>
+              <Carousel.Caption>
+                <h4>Movie View</h4>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="carousel-img" src="wireframe-profile.png" alt="wireframe for profile view" />
+              <Carousel.Caption>
+                <h4>Profile View</h4>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="carousel-img" src="wireframe-login.png" alt="wireframe for login view" />
+              <Carousel.Caption>
+                <h4>Login View</h4>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="carousel-img" src="wireframe-signup.png" alt="wireframe for signup view" />
+              <Carousel.Caption>
+                <h4>Signup View</h4>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
         </Row>
 
-        <Row>
+        <Row className="mt-5">
           <h3>2 - Creating the Components</h3>
           <Col>
             <h4>Goal:</h4>
@@ -75,6 +119,8 @@ const MovieCaseStudy = () => {
               To learn and implement React and JWT authentication to create five views (components) and to implement Bootstrap to style the user interface.<br/>
               Additionally, to create profile view, search bar and navigation components as well as add the ability for users to add and remove movies from their list of favorites.
             </p>
+          </Col>
+          <Col>
             <h4>Challenges:</h4>
             <ul>
               <li>
@@ -88,24 +134,25 @@ const MovieCaseStudy = () => {
               </li>
             </ul>
           </Col>
-          <Col>
-            <h4>Solutions:</h4>
-            <p>
-              I took courses through Codecademy to supplement my knowledge of React.<br/>
-              I also worked with my mentor, who discovered a syntax error that solved the problem with movie data not displaying: INSERTPICTURE
-            </p>
-          </Col>
+        </Row>
+        <Row className="mb-5">
+          <h4>Solutions:</h4>
+          <p>
+            I took courses through Codecademy to supplement my knowledge of React.<br/>
+            I also worked with my mentor, who discovered a syntax error that solved the problem with movie data not displaying: INSERTPICTURE
+          </p>
         </Row>
 
         <Row>
-          <Col></Col>
+          <h3>2a - Technologies</h3>
           <Col>
-            <h3>2a - Technologies</h3>
             <p>
               <span>The backbone of this application is React:</span><br/>
               A JavaScript library that uses components and state management to create user interfaces. The parent component stores data and passes it to child components. <br/><br/>
               In this application, for example, the MainView component is the landing page after logging in and loads the data for all the movies.  It then passes that data to MovieCard, MovieView and ProfileView components.
             </p>
+          </Col>
+          <Col>
             <p>
               <span>Bootstrap:</span><br/>
               A framework for responsive websites that follows a grid system to organize the layout of your user interface and has prebuilt components, utilities, and styling.<br/><br/>
@@ -124,6 +171,7 @@ const MovieCaseStudy = () => {
           <Col>
             <h4>Challenge:</h4>
             <p>Rearranging most of the return code in MainView.</p>
+            <img src="routing.png" alt="a screenshot of code" />
           </Col>
           <Col>
             <h4>Solution:</h4>
