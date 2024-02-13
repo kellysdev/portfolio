20af6d000d8593e5c55cdc809f170aefe40ed234
Kelly Scott <116105463+lladysmall@users.noreply.github.com>
2024-02-12 18:16:25 -0800

    Reduce size of mouse

--- src/components/intro.js
+++ src/components/intro.js
@@ -13,7 +13,7 @@ const Intro = () => {
           I am available for a full-time or part-time position (in person, hybrid, or remote) near Davis, CA.
         </p>
         <div className="d-flex">
-          <motion.h2 className="bi bi-mouse mouse" animate={{ x: [0, 10, 5] }} transition={{ ease: "easeOut", duration: 2 }} />
+          <motion.h4 className="bi bi-mouse mouse" animate={{ x: [0, 10, 5] }} transition={{ ease: "easeOut", duration: 2 }} />
           <span className="scroll-statement ms-4">Scroll down to check out some of my recent work.</span>
         </div>
       </Col>
