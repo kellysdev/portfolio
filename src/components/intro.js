43608b0896a4de5d50e9a97899fa4466bc7aa6c8
Kelly Scott <116105463+lladysmall@users.noreply.github.com>
2024-02-21 07:40:50 -0800

    Animate title.  Remove mouse icon  - instead change color of scroll statement.

--- src/components/intro.js
+++ src/components/intro.js
@@ -6,15 +6,16 @@ const Intro = () => {
   return (
     <Row className="intro">
       <Col className="intro-col">
-        <h1 className="intro-title">Full-Stack Developer</h1>
+        <motion.div className="intro-title" initial={{opacity: 0}} animate={{opacity: 1}} transition={{ ease: "easeOut", duration: 1.5}}>
+          Full-Stack Web Developer</motion.div>
         <p className="intro-text">          
           Hi, I'm Kelly.
           <br/>
           I am available for a full-time or part-time position (in person, hybrid, or remote) near Davis, CA.
         </p>
         <div className="d-flex">
-          <motion.h4 className="bi bi-mouse mouse" animate={{ x: [0, 10, 5] }} transition={{ ease: "easeOut", duration: 2 }} />
-          <span className="scroll-statement ms-4">Scroll down to check out some of my recent work.</span>
+          {/* <motion.h4 className="bi bi-mouse mouse" animate={{ x: [0, 10, 5] }} transition={{ ease: "easeIn", duration: 2 }} /> */}
+          <span className="scroll-statement text-info">Scroll down to check out some of my recent work.</span>
         </div>
       </Col>
     </Row>
