cdfb13b1c9d6057b2d4ed9987b32e8c88981d063
Kelly Scott <116105463+lladysmall@users.noreply.github.com>
2024-02-23 11:10:15 -0800

    Fix portfolio item responsive image sizing.

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -15,8 +15,8 @@ const Portfolio = () => {
         <motion.div initial={{opacity: 0, y: 10}} animate={{opacity: 1, y: 0}} transition={{duration: 0.5, delay: 0.1}}>
         <Col className="my-3">
           <Row>
-            <Col xs={1} sm={3} lg={4}><img src="meet.png" alt="meet app screenshot" className="portfolio-img"/></Col>
-            <Col>
+            <Col><img src="meet.png" alt="meet app screenshot" className="portfolio-img"/></Col>
+            <Col lg={8}>
               <h3>Progressive Web App</h3>
               <h6 className="technologies text-secondary">React | Serverless | AWS Lambda</h6>
               <p>Built with React using a test-driven development technique, fetches upcoming events from the Google Calendar API, utilizes OAuth2 authentication, and features data visualization enabled by use of Rechart.</p>
@@ -32,8 +32,8 @@ const Portfolio = () => {
         <motion.div initial={{opacity: 0, y: 10}} animate={{opacity: 1, y: 0}} transition={{duration: 0.5, delay: 0.2}}>
         <Col className="my-3">
           <Row>
-            <Col xs={1} sm={3} lg={4} className="portfolio-img"><img src="popopolis.png" alt="movie app screenshot" className="portfolio-img"/></Col>
-            <Col>
+            <Col className="portfolio-img"><img src="popopolis.png" alt="movie app screenshot" className="portfolio-img"/></Col>
+            <Col lg={8}>
               <h3>CRUD Single-Page Web App</h3>
               <h6 className="technologies text-secondary">MERN | API Development | Bootstrap</h6>
               <p>The server-side is built with Node, Express and MongoDB, is hosted on Heroku, and utilizes JWT authentication.  The client-side is built with React, Bootstrap, react-router, and Parcel.</p>
@@ -50,8 +50,8 @@ const Portfolio = () => {
         <motion.div initial={{opacity: 0, y: 10}} animate={{opacity: 1, y: 0}} transition={{duration: 0.5, delay: 0.3}}>
         <Col className="my-3">
           <Row>
-            <Col xs={1} sm={3} lg={4} className="portfolio-img"><img src="chat.png" alt="chat app screenshot" className="portfolio-img"/></Col>
-            <Col>
+            <Col className="portfolio-img"><img src="chat.png" alt="chat app screenshot" className="portfolio-img"/></Col>
+            <Col lg={8}>
               <h3>React Native Chat App</h3>
               <h6 className="technologies text-secondary">Google Firebase | Expo | GiftedChat</h6>
               <p>A cross-platform chat application that allows users to share images and their locationas well as to choose the background color for the chat screen.</p>
@@ -66,8 +66,8 @@ const Portfolio = () => {
         <motion.div initial={{opacity: 0, y: 10}} animate={{opacity: 1, y: 0}} transition={{duration: 0.5, delay: 0.4}}>
         <Col className="my-3">
           <Row>
-            <Col xs={1} sm={3} lg={4} className="portfolio-img"><img src="pokedex.png" alt="pokedex app screenshot" className="portfolio-img"/></Col>
-            <Col>
+            <Col className="portfolio-img"><img src="pokedex.png" alt="pokedex app screenshot" className="portfolio-img"/></Col>
+            <Col lg={8}>
               <h3>API Fetch App</h3>
               <h6 className="technologies text-secondary">JavaScript | JQuery | Bootstrap</h6>
               <p>Loads data from an external API, the PokeAPI, and enables the viewing of data points in detail.</p>
