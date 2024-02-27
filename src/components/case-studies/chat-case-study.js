ad90f89017d0de081e6c370dd46678e7abafe98b
Kelly Scott <116105463+lladysmall@users.noreply.github.com>
2024-02-26 17:46:03 -0800

    Create case-studies folder in components folder and move case studies.

---
+++ src/components/case-studies/chat-case-study.js
@@ -0,0 +1,30 @@
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+const ChatCaseStudy = () => {
+  return (
+    <Row>
+      <Col>
+
+{/* hero */}
+      <Row>
+          <header className="meet-hero has-bg-img" style={{ paddingLeft: 0 }}>
+            <div className="p-3 text-center bg-image">
+              <div className="mask">
+                <div className="d-flex justify-content-center align-items-center h-100">
+                  <div className="text-white">
+                    <h1 className="mt-5 text-dark">Case Study</h1>
+                    <h4 className="mb-5 text-dark">Serverless Progressive Web Application</h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </header>
+        </Row>
+      
+      </Col>
+    </Row>
+  );
+};
+
+export default ChatCaseStudy;
--- src/components/chat-case-study.js
+++
@@ -1,9 +0,0 @@
-import Row from "react-bootstrap/Row";
-
-const ChatCaseStudy = () => {
-  return (
-    <Row></Row>
-  );
-};
-
-export default ChatCaseStudy;
