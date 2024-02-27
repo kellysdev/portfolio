88074e803aa672a33b2da7ce62353d64a94efedb
Kelly Scott <116105463+lladysmall@users.noreply.github.com>
2024-02-26 19:13:33 -0800

    Populate component with text and basic layout.

--- src/components/case-studies/chat-case-study.js
+++ src/components/case-studies/chat-case-study.js
@@ -21,6 +21,108 @@ const ChatCaseStudy = () => {
             </div>
           </header>
         </Row>
+
+{/* overview */}
+        <Row className="mt-4">
+          <Col>
+            <h3>Overview</h3>
+            <p>
+              An accessible cross-platform chat application that allows users to share images and their locations as well as to choose the background color of the chat screen.
+            </p>
+          </Col>
+          <Col>
+            <h3>Purpose & Context</h3>
+            <p>
+              This app was built while taking the CareerFoundry Full-Stack Web Development course to learn React Native, Expo and Google Firestore Database.
+            </p>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h3>Objective</h3>
+            <p>
+              To build a chat app for mobile devices using React Native.  The chat app will provide users with a chat interface and options to share images and their location.
+            </p>
+          </Col>
+        </Row>
+
+{/* tech used */}
+        <Row className="mt-5 mb-2"><h3>Technology Used</h3></Row>
+        <Row className="d-flex">
+          <Col>
+            <p><b>React Native</b> allows for cross platform development using JavaScript and React.  React Native Navigation is a package that was used to allow navigation between the start screen and the chat screen.</p>
+          </Col>
+          <Col>
+            <p><b>Expo</b> is a software development kit that allows developers to create applications with React that will work on Android and iOS.  Expo Go is an app that I used to test the application on my iPhone.</p>
+          </Col>
+          <Col>
+            <p><b>Android Studio</b> is an emulator that allows developers to test their application on an Android device if they don’t own one.</p>
+          </Col>
+          <Col>
+            <p><b>GiftedChat</b> is a chat user interface package for React Native that implements chat functionality with customization.</p>
+          </Col>
+          <Col>
+            <p><b>AsyncStorage</b> is a package recommended by Expo and React Native.  It allows developers to store data locally on a mobile device, similar to how data is stored in localStorage on a web browser.</p>
+          </Col>
+          <Col>
+            <p><b>Google Firebase</b> is a web and mobile app development platform which provides services related to app authentication, data storage, and security.  It allows users to log in anonymously in this chat app and its Firestore Database was used to store the messages and images sent in the chat.</p>
+          </Col>
+        </Row>
+        <Row>
+          <img className="built-with-firebase" src="https://firebase.google.com/static/downloads/brand-guidelines/SVG/logo-built_black.svg" alt="Built with Firebase" />
+        </Row>
+
+{/* dev process */}
+        <Row className="my-5">
+          <h3>Development Process</h3>
+          <ol>
+            <li>
+              <ul>React Native and Expo
+                <li>
+                  An Expo application was created from a blank Expo template and navigation set up using React Native Navigation.
+                </li>
+              </ul>
+            </li>        
+            <li>
+              <ul>Design Specifications
+                <li>
+                  The design of the start screen was specified by the project brief with image and color assets provided.
+                </li>
+              </ul>
+            </li>
+            <li>
+              <ul>Chat Functionality & Accessibility
+                <li>
+                  The GiftedChat library was used for the main chat functionality.  It provides an interface that users expect of a chat application and allowed for customization.  This customization allowed for the implementation of an action button (from React Native ActionSheet) which, when pressed, opens a menu from which users can choose to send a picture, take a picture with the device camera to send, or to send their location to chat.
+                </li>
+                <li>
+                  Many of the functions of mobile devices use buttons without text on them so it is important to include accessibility roles for those buttons and similar items.
+                </li>
+              </ul>
+            </li>
+            <li>
+              <ul>Real Time Applications & Data Storage
+                <li>
+                  An effective chat app will show users new messages as soon as they are sent.  This function was made possible with Google Firebase websockets protocol.  The messages are also saved to Google Firestore cloud storage.
+                </li>
+                <li>
+                  Chat applications are useful when the user can access sent messages and images even when their device doesn’t have internet connection.  This feature was enabled by the React Native AsyncStorage package.
+                </li>
+              </ul>
+            </li>
+          </ol>
+        </Row>
+
+{/* final product */}
+        <Row className="my-5 credits">
+          <h3>Final Product</h3>
+          <p>
+              Lead Developer: Kelly Scott<br />
+              Mentor: Vini De Antoni<br />
+              Tutor: Jason Early
+            </p>
+            <a href="https://github.com/lladysmall/chat-app">GitHub Repository</a>
+        </Row>
       
       </Col>
     </Row>
