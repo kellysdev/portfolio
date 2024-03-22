3127dd530aab51ae85af778c1a607387943f732f
Kelly Scott <116105463+kellysdev@users.noreply.github.com>
2024-03-22 13:32:57 -0700

    Center and reduce width of contact form and title.

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,5 +1,6 @@
 import { useRef } from "react";
 import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import emailjs from "@emailjs/browser";
@@ -25,23 +26,25 @@ const Contact = () => {
   };
 
   return (
-    <Row className="my-5">
-      <motion.h1 initial={{opacity: 0, y: 10}} animate={{opacity: 1, y: 0}} transition={{duration: 0.5}}>Contact Me</motion.h1>
-      <Form ref={form} onSubmit={handleSubmit}>
-        <Form.Group>
-          <Form.Label className="mt-3">Name</Form.Label>
-          <Form.Control type="text" name="from_name"/>
-        </Form.Group>
-        <Form.Group>
-          <Form.Label className="mt-3">Email</Form.Label>
-          <Form.Control type="email" name="from_email" />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label className="mt-3">Message</Form.Label>
-          <Form.Control as="textarea" rows={5} name="message" />
-        </Form.Group>
-        <Button className="contact-button my-3" variant="outline-light" type="submit">Submit</Button>
-      </Form>
+    <Row className="my-5 d-flex justify-content-center">
+      <Col md={8} className="">
+        <motion.h1 initial={{opacity: 0, y: 10}} animate={{opacity: 1, y: 0}} transition={{duration: 0.5}}>Contact Me</motion.h1>
+        <Form ref={form} onSubmit={handleSubmit}>
+          <Form.Group>
+            <Form.Label className="mt-3">Name</Form.Label>
+            <Form.Control type="text" name="from_name"/>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label className="mt-3">Email</Form.Label>
+            <Form.Control type="email" name="from_email" />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label className="mt-3">Message</Form.Label>
+            <Form.Control as="textarea" rows={5} name="message" />
+          </Form.Group>
+          <Button className="contact-button my-3" variant="outline-light" type="submit">Submit</Button>
+        </Form>
+      </Col>
     </Row>
   );
 };
