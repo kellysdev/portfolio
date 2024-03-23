808e5bb85efd1b4d47495ee831bde9ef96d8fcdd
Kelly Scott <116105463+kellysdev@users.noreply.github.com>
2024-03-23 11:00:53 -0700

    Left align and increase top margin of titles in development process.
    Easier to read.

--- src/components/case-studies/chat-case-study.js
+++ src/components/case-studies/chat-case-study.js
@@ -74,21 +74,21 @@ const ChatCaseStudy = () => {
           <Col>
           
             <Row>
-              <h5 className="text-center mt-2">React Native and Expo</h5>
+              <h5 className="mt-3">React Native and Expo</h5>
               <p>
                 An Expo application was created from a blank Expo template and navigation set up using React Native Navigation.
               </p>
             </Row>
 
             <Row>
-              <h5 className="text-center mt-2">Design Specifications</h5>
+              <h5 className="mt-3">Design Specifications</h5>
               <p>
                 The design of the start screen was specified by the project brief with image and color assets provided.
               </p>
             </Row>
 
             <Row>
-              <h5 className="text-center mt-2">Chat Functionality & Accessibility</h5>
+              <h5 className="mt-3">Chat Functionality & Accessibility</h5>
               <p>
                 The GiftedChat library was used for the main chat functionality.  It provides an interface that users expect of a chat application and allowed for customization.<br /><br />
                 This customization allowed for the implementation of an action button (from React Native ActionSheet) which, when pressed, opens a menu from which users can choose to send a picture, take a picture with the device camera to send, or to send their location to chat.<br /><br />
@@ -98,7 +98,7 @@ const ChatCaseStudy = () => {
             </Row>
 
             <Row>
-              <h5 className="text-center mt-2">Real Time Applications & Data Storage</h5>
+              <h5 className="mt-3">Real Time Applications & Data Storage</h5>
               <p>
                 An effective chat app will show users new messages as soon as they are sent.  This function was made possible with Google Firebase websockets protocol.  The messages are also saved to Google Firestore cloud storage.<br />
                 Chat applications are useful when the user can access sent messages and images even when their device doesn’t have internet connection.  This feature was enabled by the React Native AsyncStorage package.
