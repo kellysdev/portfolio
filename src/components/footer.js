9d0c1dd0e7397d18bec4e4cde66c171ba57d8ffb
Kelly Scott <116105463+kellysdev@users.noreply.github.com>
2024-03-25 12:19:36 -0700

    Add aria-labels to links and change links to include github, linkedin and twitter

--- src/components/footer.js
+++ src/components/footer.js
@@ -11,34 +11,28 @@ const Footer = () => {
         <Row>
 {/* github */}
           <Col xs={1} className="ms-auto">
-            <a href="https://github.com/kellysdev" target="_blank" rel="noreferrer">
+            <a href="https://github.com/kellysdev" aria-label="Link to GitHub profile" target="_blank" rel="noreferrer">
               <h4><i className="bi bi-github text-white"></i></h4>
             </a>
           </Col>
 {/* linkedin */}
           <Col xs={1} className="mx-1">
-            <a href="https://www.linkedin.com/in/kellysdev/" target="_blank" rel="noreferrer">
+            <a href="https://www.linkedin.com/in/kellysdev/" aria-label="Link to LinkedIn profile" target="_blank" rel="noreferrer">
               <h4><i className="bi bi-linkedin text-white" ></i></h4>
             </a>
           </Col>
-{/* medium */}
-          <Col xs={1} className="mx-1">
-            <a href="https://medium.com/@kellysdev" target="_blank" rel="noreferrer">
-              <h4><i class="bi bi-medium text-white"></i></h4>
-            </a>
-          </Col>
-{/* twitter */}
-          <Col xs={1} className="me-auto">
-            <a href="https://twitter.com/lladysmall" target="_blank" rel="noreferrer">
-              <h4><i class="bi bi-twitter-x text-white"></i></h4>
-            </a>
-          </Col>
 {/* instagram */}
-          {/* <Col xs={1} className="me-auto">
-            <a href="https://www.instagram.com/kellysdev/" target="_blank" rel="noreferrer">
+          {/* <Col>
+            <a href="https://www.instagram.com/kellysdev/" aria-label="Link to Instagram account" target="_blank" rel="noreferrer">
               <h4><i className="bi bi-instagram text-white" ></i></h4>
             </a>
           </Col> */}
+{/* twitter */}
+          <Col xs={1} className="me-auto">
+            <a href="https://twitter.com/lladysmall" aria-label="Link to Twitter profile" target="_blank" rel="noreferrer">
+              <h4><i class="bi bi-twitter-x text-white"></i></h4>
+            </a>
+          </Col>
         </Row>
       </Col>
     </Row>
