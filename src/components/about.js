fe5ffec250d7f5221e766d335299a05024e07859
Kelly Scott <116105463+kellysdev@users.noreply.github.com>
2024-06-07 15:17:46 -0700

    Change order of two paragraphs.

--- src/components/about.js
+++ src/components/about.js
@@ -20,11 +20,11 @@ const About = () => {
 {/* text */}
         <Col md={8} className="mb-5">
           <p className="about-text">
-            Through my background in brewing and research, I have developed an analytical and systematic approach to my work. 
-            Writing standard operating procedures and developing curriculum for brewing courses has led me to focus on comprehension and user experience.
-            <br/><br/>
             I am a problem solver who is big on taking the initiative and has a passion for project management.
             I enjoy the structure and creativity of coding and find beauty in the way that a website can evoke as much emotion as a physical space.
+            <br/><br/>
+            Through my background in brewing and research, I have developed an analytical and systematic approach to my work. 
+            Writing standard operating procedures and developing curriculum for brewing courses has led me to focus on comprehension and user experience.
             <br/><br/>
             When I'm not coding, I am spending time with my boyfriend and our dog - often playing video games, tinkering with a mechanical keyboard, or gardening.
             <br/><br/>
