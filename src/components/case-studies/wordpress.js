b5a64426a1b405b2b9746d1f852ecdfefcbb1d01
Kelly Scott <116105463+kellysdev@users.noreply.github.com>
2024-08-22 15:31:36 -0700

    Create wordpress case study.

---
+++ src/components/case-studies/wordpress.js
@@ -0,0 +1,13 @@
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+const WordPress = () => {
+  return (
+    <Row>
+      <Col></Col>
+    </Row>
+  );
+
+};
+
+export default WordPress;
