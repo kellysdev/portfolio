c2e88c9b76bb4b796be8ab8c3a8544ff9abe75c6
Kelly Scott <116105463+kellysdev@users.noreply.github.com>
2024-08-25 17:18:42 -0700

    Create circuit timer case study.

---
+++ src/components/case-studies/circuit-timer-casestudy.js
@@ -0,0 +1,13 @@
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+const CircuitTimer = () => {
+  return (
+    <Row>
+      <Col>      
+      </Col>
+    </Row>
+  )
+};
+
+export default CircuitTimer;
