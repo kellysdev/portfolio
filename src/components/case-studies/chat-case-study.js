7bdf258bbe72c0c100c3528f1aef60d71e3694be
Kelly Scott <116105463+kellysdev@users.noreply.github.com>
2024-12-09 17:35:28 -0800

    Shorten overview, center text and headings, and reorganize development.

--- src/components/case-studies/chat-case-study.js
+++ src/components/case-studies/chat-case-study.js
@@ -23,32 +23,18 @@ const ChatCaseStudy = () => {
         </Row>
 
 {/* overview */}
-        <Row className="mt-4">
+        <Row className="mt-4 text-center">
           <Col>
             <h3>Overview</h3>
             <p>
-              An accessible cross-platform chat application that allows users to share images and their locations as well as to choose the background color of the chat screen.
-            </p>
-          </Col>
-          <Col>
-            <h3>Purpose & Context</h3>
-            <p>
-              This app was built while taking the CareerFoundry Full-Stack Web Development course to learn React Native, Expo and Google Firestore Database.
-            </p>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <h3>Objective</h3>
-            <p>
-              To build a chat app for mobile devices using React Native.  The chat app will provide users with a chat interface and options to share images and their location.
+            The goal was to build an accessible cross-platform chat application that allows users to share images and their locations as well as to choose the background color of the chat screen.  This app was built while taking the CareerFoundry Full-Stack Web Development course.
             </p>
           </Col>
         </Row>
 
 {/* tech used */}
         <Row className="mt-5 mb-2">
-          <h3>Technology Used</h3>
+          <h3 className="text-center">Technology Used</h3>
           <Col className="d-flex-row">
           
           <Row className="my-3 pt-2 mx-auto chat-left">
@@ -74,39 +60,22 @@ const ChatCaseStudy = () => {
         </Row>
 
 {/* dev process */}
-        <Row className="my-5">
-          <h3>Development Process</h3>
+        <Row className="my-5 text-center">
+          <h3>Development</h3>
           <Col>
           
             <Row>
-              <h5 className="mt-3">React Native and Expo</h5>
+              <h5 className="mt-3">Set Up</h5>
               <p>
-                An Expo application was created from a blank Expo template and navigation set up using React Native Navigation.
-              </p>
-            </Row>
-
-            <Row>
-              <h5 className="mt-3">Design Specifications</h5>
-              <p>
-                The design of the start screen was specified by the project brief with image and color assets provided.
+                An Expo application was created from a blank Expo template and navigation set up using React Native Navigation.  The design of the start screen was specified by the project brief with image and color assets provided.                  An effective chat app will show users new messages as soon as they are sent.  This function was made possible with Google Firebase websockets protocol.  The messages are also saved to Google Firestore cloud storage.               Chat applications are useful when the user can access sent messages and images even when their device doesn’t have internet connection.  This feature was enabled by the React Native AsyncStorage package.
               </p>
             </Row>
 
             <Row>
               <h5 className="mt-3">Chat Functionality & Accessibility</h5>
               <p>
-                The GiftedChat library was used for the main chat functionality.  It provides an interface that users expect of a chat application and allowed for customization.<br /><br />
-                This customization allowed for the implementation of an action button (from React Native ActionSheet) which, when pressed, opens a menu from which users can choose to send a picture, take a picture with the device camera to send, or to send their location to chat.<br /><br />
-                Many of the functions of mobile devices use buttons without text on them so it is important to include accessibility roles for those buttons and similar items.<br /><br />
-                In this case, the Touchable Opacity utility was used instead of a button because it was more customizable.  Each Touchable Opacity received accessibilityRole="button" and, when there wasn't text in the Touchable Opacity, it received an appropriate accessibilityLabel and accessibilityHint.
-              </p>
-            </Row>
-
-            <Row>
-              <h5 className="mt-3">Real Time Applications & Data Storage</h5>
-              <p>
-                An effective chat app will show users new messages as soon as they are sent.  This function was made possible with Google Firebase websockets protocol.  The messages are also saved to Google Firestore cloud storage.<br />
-                Chat applications are useful when the user can access sent messages and images even when their device doesn’t have internet connection.  This feature was enabled by the React Native AsyncStorage package.
+                The GiftedChat library was used for the main chat functionality.  It provides an interface that users expect of a chat application and allowed for customization.  This customization allowed for the implementation of an action button (from React Native ActionSheet) which, when pressed, opens a menu from which users can choose to send a picture, take a picture with the device camera to send, or to send their location to chat.<br />
+                Many of the functions of mobile devices use buttons without text on them so it is important to include accessibility roles for those buttons and similar items.  In this case, the Touchable Opacity utility was used instead of a button because it was more customizable.  Each Touchable Opacity received accessibilityRole="button" and, when there wasn't text in the Touchable Opacity, it received an appropriate accessibilityLabel and accessibilityHint.
               </p>
             </Row>
 
@@ -115,7 +84,7 @@ const ChatCaseStudy = () => {
 
 {/* final product */}
         <Row>
-          <h3>Final Product</h3>
+          <h3 className="text-center">Final Product</h3>
           <div className="d-flex">
             <Col lg={4} className="mx-auto">
               <img className="final-chat-screenshot" src="chat-start.png" alt="chat app screenshot of start screen"/>
